1ea854acea507db1722663682cf2f0867a573771
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-08 09:57:17 -0600

    create test for user story 2

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -30,4 +30,38 @@ RSpec.describe "Plots index" do
       expect(page).to_not have_content(plant2.name)
     end
   end
+
+  it 'can remove plant from plot without deleteing plant' do
+    garden1 = Garden.create!(name: 'Turing Community Garden', organic: true)
+    plot1 = garden1.plots.create!(number: 25, size: "Large", direction: "East")
+    plot2 = garden1.plots.create!(number: 26, size: "Small", direction: "West")
+    plant1 = Plant.create!(name: "rose", description: "a rose", days_to_harvet: 10)
+    plant2 = Plant.create!(name: "cucumber", description: "a cucumber", days_to_harvet: 15)
+    plantplot1 = PlantPlot.create(plant: plant1, plot: plot1)
+    plantplot2 = PlantPlot.create(plant: plant2, plot: plot1)
+    plantplot4 = PlantPlot.create(plant: plant1, plot: plot2)
+
+    visit '/plots'
+
+    within "#plots-#{plot1.id}" do
+      expect(page).to have_content(plant1.name)
+      expect(page).to have_content(plant2.name)
+      expect(page).to have_link("Remove #{plant1.name}")
+      expect(page).to have_link("Remove #{plant2.name}")
+    end
+
+    click_link("Remove #{plant1.name}")
+    expect(current_path).to eq('/plots')
+
+    within "#plots-#{plot2.id}" do
+      expect(page).to have_content(plant1.name)
+    end
+
+    within "#plots-#{plot1.id}" do
+      expect(page).to_not have_content(plant1.name)
+      expect(page).to have_content(plant2.name)
+      expect(page).to_not have_link("Remove #{plant1.name}")
+      expect(page).to have_link("Remove #{plant2.name}")
+    end
+  end
 end

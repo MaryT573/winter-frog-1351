86aaea10ba88541e4d4a250021dfbe99bd324a9b
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-08-08 09:36:02 -0600

    create tests for user story 1

---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Plots index" do
+  it 'has a list of all plots and thier associated plants' do
+    garden1 = Garden.create!(name: 'Turing Community Garden', organic: true)
+    plot1 = garden1.plots.create!(number: 25, size: "Large", direction: "East")
+    plot2 = garden1.plots.create!(number: 26, size: "Small", direction: "West")
+    plant1 = Plant.create!(name: "rose", description: "a rose", days_to_harvet: 10)
+    plant2 = Plant.create!(name: "cucumber", description: "a cucumber", days_to_harvet: 15)
+    plant3 = Plant.create!(name: "pumpkin", description: "a pumpkin", days_to_harvet: 20)
+    plantplot1 = PlantPlot.create(plant: plant1, plot: plot1)
+    plantplot2 = PlantPlot.create(plant: plant2, plot: plot1)
+    plantplot3 = PlantPlot.create(plant: plant3, plot: plot2)
+    plantplot4 = PlantPlot.create(plant: plant1, plot: plot2)
+
+    visit '/plots'
+    expect(current_path).to eq('/plots')
+
+    within "#plots-#{plot1.id}" do
+      expect(page).to have_content(plot1.number)
+      expect(page).to have_content(plant1.name)
+      expect(page).to have_content(plant2.name)
+      expect(page).to_not have_content(plant3.name)
+    end
+
+    within "#plots-#{plot2.id}" do
+      expect(page).to have_content(plot2.number)
+      expect(page).to have_content(plant1.name)
+      expect(page).to have_content(plant3.name)
+      expect(page).to_not have_content(plant2.name)
+    end
+  end
+end
